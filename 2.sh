62d8414038ef7b209713d2114aecd5c9e465e19a
mguazzardo <mguazzardo76@gmail.com>
2023-01-13 05:22:58 -0300

    'change2'

---
+++ 2.sh
@@ -0,0 +1,11 @@
+#ctrl-ins
+#shift-ins
+
+    
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+
