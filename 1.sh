2152a8aaaa48f4e0cd8abcbe3aa23e2368262005
tercemundo <94326261+tercemundo@users.noreply.github.com>
2023-01-13 05:15:42 -0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,9 @@
+#ctrl-ins
+#shift-ins
+
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+    
+
+
+
+
---
+++ 2.sh
@@ -0,0 +1,10 @@
+#ctrl-ins
+#shift-ins
+
+    
+
+# 2. Initialize cluster networking:
+
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+
+
